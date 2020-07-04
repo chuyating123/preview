4857eb22afc49d348f7809f94e9646bd60a80233
chuyating123 <761181414@qq.com>
2020-07-04 21:56:16 +0800

    vue-app

--- vue-app/src/views/Book.vue
+++ vue-app/src/views/Book.vue
@@ -1,12 +1,24 @@
 <template>
     <div>
-
+   <comment-book></comment-book>
     </div>
 </template>
 
 <script>
+import CommentBook from '../components/CommentBook'
     export default {
-        
+         data(){
+            return{
+                imgs:[
+                    "https://img3.doubanio.com/lpic/s24468373.jpg",
+                    "https://img3.doubanio.com/lpic/s27102925.jpg",
+                    "https://img3.doubanio.com/lpic/s6989253.jpg"
+                ]
+            }
+        },
+        components:{
+            CommentBook
+        }
     }
 </script>
 
--- vue-app/src/views/MusicDetail.vue
+++ vue-app/src/views/MusicDetail.vue
@@ -1,28 +1,40 @@
 <template>
-    <div>
-    音乐详情页
-    </div>
+  <div>
+    <aplayer
+      :music="{
+                title: 'secret base~君がくれたもの~',
+                artist: 'Silent Siren',
+                src: 'http://m10.music.126.net/20200301002801/5112efffc3818e8b88a74e36a5352e94/ymusic/b1c4/b5de/74d0/9158ae4873e10b743790320db9ef9b29.mp3',
+                pic: 'http://p1.music.126.net/QHw-RuMwfQkmgtiyRpGs0Q==/102254581395219.jpg',
+                lrc:'data/xxx.a.lrc'
+            }"
+      :list="musicdata"
+      :showLrc="true"
+    />
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-    export default {
-        data(){
-            return{
-            musicdata:[]
-            }
-        },
-        created(){
-          axios.get('data/musicdata.json')
-          .then(res=>{
-              this.musicdata=res.data.musicData
-          }).catch(err=>{
-              console.log(err)
-          })
-        }
-    }
+import axios from "axios";
+import Aplayer from "vue-aplayer";
+export default {
+  data() {
+    return {
+      musicdata: []
+    };
+  },
+  created() {
+    axios
+      .get("data/musicdata.json")
+      .then(res => {
+        this.musicdata = res.data.musicData;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
