a562f64d7a7be28ec4305a75e8795a3a938ddb9b
chuyating123 <761181414@qq.com>
2020-07-08 15:46:15 +0800

    react-project

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route,Link} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+     <Link to="/">首页</Link>
+     <Link to="/About">关于我们</Link>
+     </div>
+     <div>
+      <Route path="/" component={Home}></Route>
+      <Route path="/About" component={About}></Route>
+     </div>
     </div>
   );
 }
