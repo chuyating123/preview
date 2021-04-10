5ebcd0e461002d47e5d7c8093200e940c7822cdb
chuyating123 <761181414@qq.com>
2021-04-10 16:18:34 +0800

    vue-app

--- NewPreview/Vue/vue-app/src/App.vue
+++ NewPreview/Vue/vue-app/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">首页</router-link> |
+      <router-link to="/detail">详情</router-link>
+      <router-link to="/test">测试</router-link>
     </div>
     <router-view/>
   </div>
